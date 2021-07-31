bb874dea91e7917e070d9212ee9fdd16498c2142
Fremen1990 <fremen1990@gmail.com>
2021-07-31 21:10:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { gameSubject } from "./Game";
+import Board from "./Board";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [board, setBoard] = useState([]);
+  useEffect(() => {
+    const subscribe = gameSubject.subscribe((game) => setBoard(game.board));
+    return () => subscribe.unsubscribe();
+  }, []);
+
+  return (<>
+    <h1 className="game-name">DEVTHOMAS CHESS GAME!!! :)</h1>
+    <div className="container">
+
+      <div className="board-container">
+
+        <Board board={board} />
+
+      </div>
+
+
+
     </div>
+  </>
+
   );
 }
 
---
+++ src/Board.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import BoardSquare from './BoardSquare'
+
+
+export default function Board({ board }) {
+
+    function getXYPosition(i) {
+        const x = i % 8 // modulo from 8 collumns
+        const y = Math.abs(Math.floor(i / 8) - 7) // math absolute -> floor root minus rest of collumns
+        return { x, y }
+    }
+
+    function isBlack(i) {
+        const { x, y } = getXYPosition(i)
+        return (x + y) % 2 === 1 // modulo from X+Y === then is black
+    }
+
+
+    return (
+        <div className="board">
+            {board.flat().map((piece, i) =>
+
+                <div key={i} className="square">
+                    {/* <p>{JSON.stringify(piece)}</p> */}
+                    <BoardSquare piece={piece} black={isBlack(i)} />
+                </div>
+
+            )}
+        </div>
+    )
+}
