06026051610eaff0c126f9643f37bde507ef4eb4
Fremen1990 <fremen1990@gmail.com>
2021-08-12 16:47:36 +0200

    detecting game over

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { gameSubject, initGame } from "./Game";
+import { gameSubject, initGame, resetGame } from "./Game";
 import Board from "./Board";
-
 
 function App() {
   const [board, setBoard] = useState([]);
+
+  const [isGameOver, setIsGameOver] = useState();
+  const [result, setResult] = useState();
+
   useEffect(() => {
-    initGame()
-    const subscribe = gameSubject.subscribe((game) => setBoard(game.board));
+    initGame();
+    const subscribe = gameSubject.subscribe((game) => {
+      setBoard(game.board);
+      setIsGameOver(game.isGameOver);
+      setResult(game.result);
+    });
     return () => subscribe.unsubscribe();
   }, []);
 
+  return (
+    <>
+      <h1 className="game-name">DEVTHOMAS CHESS GAME!!! :)</h1>
 
+      <div className="container">
+        {isGameOver && (
+          <h2 className="vertical-text">
+            GAME OVER
+            <button onClick={resetGame}>
+              <span className="vertical-text">NEW GAME</span>
+            </button>
+          </h2>
+        )}
 
-  return (<>
-    <h1 className="game-name">DEVTHOMAS CHESS GAME!!! :)
-
-    </h1>
-
-
-    <div className="container">
-
-      <div className="board-container">
-
-        <Board board={board} />
-
+        <div className="board-container">
+          <Board board={board} />
+        </div>
+        {result && <p className="vertical-text">{result}</p>}
       </div>
-
-
-
-    </div>
-  </>
-
+    </>
   );
 }
 
